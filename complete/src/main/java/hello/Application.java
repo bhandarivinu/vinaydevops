103b79ca6470cbf76c98754b092237f5f5815b56
bhandarivinu <bhandarivinu@gmail.com>
2022-09-25 18:02:45 +0530

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Hello DevOps World";
 	}
 
 	public static void main(String[] args) {
